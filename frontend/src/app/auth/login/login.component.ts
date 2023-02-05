862d0c39ca495b3a5ef2286c14f3ed0c3943a0f5
Ankit Meshram <ankitdm69@gmail.com>
2023-02-05 15:44:26 +0530

    FP-7 Login, Register, Reset Password html design

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['../auth.component.css','./login.component.css']
 })
 export class LoginComponent {
 
--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['../auth.component.css', './register.component.css']
 })
 export class RegisterComponent {
 
