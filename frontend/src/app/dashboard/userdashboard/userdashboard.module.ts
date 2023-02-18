9582d81ed14a1c944f3f574f066a4c747702e14e
Ankit Meshram <ankitdm69@gmail.com>
2023-02-18 20:35:39 +0530

    FP-10 Dashboard Component Creation

--- frontend/src/app/dashboard/userdashboard/userdashboard.module.ts
+++ frontend/src/app/dashboard/userdashboard/userdashboard.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { UserDashboardRoutingModule } from './userdashboard-routing.module';
 import { UserdashboardComponent } from './userdashboard.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { UsersComponent } from './users/users.component';
+import { FeedsComponent } from './feeds/feeds.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
   declarations: [
     UserdashboardComponent,
+    AnalyticsComponent,
+    ProjectsComponent,
+    UsersComponent,
+    FeedsComponent,
+    ProjectDetailsComponent,
 
   ],
   imports: [
