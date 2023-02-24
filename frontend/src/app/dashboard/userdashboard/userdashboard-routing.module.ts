dc1c1f124c8517c3a4ae1d567c53b8a38c690cce
Ankit Meshram <ankitdm69@gmail.com>
2023-02-24 18:07:18 +0530

    FP-15 Created routing for project list page

--- frontend/src/app/dashboard/userdashboard/userdashboard-routing.module.ts
+++ frontend/src/app/dashboard/userdashboard/userdashboard-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
 import { UserdashboardComponent } from './userdashboard.component';
 
 
@@ -7,7 +8,14 @@ const routes: Routes = [
   {
     path: '',
     component: UserdashboardComponent,
+    children: [
+      {
+      path: 'projects',
+      component: ProjectsComponent
+    }
+  ]
   },
+
 ];
 
 @NgModule({
