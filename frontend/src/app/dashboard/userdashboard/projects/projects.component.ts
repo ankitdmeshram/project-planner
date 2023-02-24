fd313192bcdf5a9573442111892556f466696f70
Ankit Meshram <ankitdm69@gmail.com>
2023-02-24 18:09:58 +0530

    FP-16 Created Design for projects

--- frontend/src/app/dashboard/userdashboard/projects/projects.component.ts
+++ frontend/src/app/dashboard/userdashboard/projects/projects.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['../userdashboard.component.css','./projects.component.css']
 })
 export class ProjectsComponent {
+
+  showAddProjectForm:boolean = false;
 
 }
