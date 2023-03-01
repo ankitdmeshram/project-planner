c49ad0670d33c742d178fecf76004bddbec9083a
Ankit Meshram <ankitdm69@gmail.com>
2023-03-01 12:48:54 +0530

    multiple changes

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -52,16 +52,16 @@ class Kernel extends HttpKernel
      *
      * @var array<string, class-string|string>
      */
-    protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+	protected $routeMiddleware = [
+        'auth'          => \Illuminate\Auth\Middleware\Authenticate::class,
+        'auth.basic'    => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'bindings'      => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'signed' => \App\Http\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'can'           => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest'         => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'signed'        => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle'      => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'cors'          => \App\Http\Middleware\Cors::class, // added
     ];
+
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\projectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['cors'])->group(function () {
+
+// Route::get("projects", [projectController::class, 'getData']);
+Route::post("projects", [projectController::class, 'projects']);
+
+Route::post("project/create_project", [projectController::class, 'createProject']);
+
+Route::post("project/update_project", [projectController::class, 'updateProject']);
+
+Route::post("project/delete_project", [projectController::class, 'deleteProject']);
+
+Route::post("project/details", [projectController::class, 'projectDetails']);
+
+// Route::get("projects", [projectController::class, 'getData']);
+Route::post("projects_details", [projectController::class, 'project_details']);
+
+Route::post("projects_details/create_task", [projectController::class, 'createTask']);
+
+Route::post("projects_details/update_task", [projectController::class, 'updateTask']);
+
+Route::post("projects_details/delete_task", [projectController::class, 'deleteTask']);
+
 });
